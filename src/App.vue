5b204165bfc0ac29183461e763512080e6083508
hilmizul <netspytux@gmail.com>
2022-12-18 09:52:42 +0700

    title

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <div class="flex align-middle flex-wrap">
       <img src="@/assets/logo.png" alt="" class="w-34 h-36 mb-10">
       <ul class="list-none mt-5">
-        <li><h2 class="text-white font-bold text-4xl ml-7 ">Buku Tamu</h2></li>
+        <li><h2 class="text-white font-bold text-4xl ml-7 ">Buku Tamu &bull; EXPlore BIJB Jawa Barat</h2></li>
         <li><h2 class=" font-semibold text-slate-200 ml-7 text-xl ">SMKN 4 Tasikmalaya</h2></li>
         <li><h3 class="font-semibold text-slate-300 ml-7 text-xl">Jl.Depok nomor 31</h3></li>
       </ul>
